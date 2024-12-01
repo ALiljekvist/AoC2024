44fd88daa7ec042a26327538b2bad01e2d5bd628
Anton Almqvist <jantonalmqvist@gmail.com>
2024-12-01 06:08:36 +0100

    Day 1 fast solution
    The solution I did just to finish day 1 quickly. Will probably update
    this to a more elegant solution that reads easier and runs faster.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,30 @@
 use aoc_tools::input::input::read_lines;
 
 fn main() {
-    let input = read_lines::<i64>("input.txt").unwrap();
-    println!("part1: {:?}", input);
+    let input = read_lines::<String>("input.txt").unwrap();
+    let mut l1: Vec::<i64> = Vec::new();
+    let mut l2: Vec::<i64> = Vec::new();
+    for l in input {
+        let nums: Vec<i64> = l.split("   ").into_iter().map(|x| x.parse().unwrap()).collect();
+        l1.push(nums[0]);
+        l2.push(nums[1]);
+    }
+    l1.sort();
+    l2.sort();
+    let mut p1 = 0;
+    for i in 0..l1.len() {
+        p1 += (l1[i] - l2[i]).abs();
+    }
+    println!("part1: {}", p1);
+    let mut p2 = 0;
+    for i in 0..l1.len() {
+        let mut cntr = 0;
+        for j in 0..l2.len() {
+            if l1[i] == l2[j] {
+                cntr += 1;
+            }
+        }
+        p2 += cntr * l1[i];
+    }
+    println!("part2: {}", p2);
 }
