38d68322474e7519c8cab7ba390dae1c9d03ca48
Anton Almqvist <jantonalmqvist@gmail.com>
2024-12-01 22:44:33 +0100

    Updated solution that wraps the logic to oneliners
    Made use of the zip function to iterate the two sorted lists at once,
    as well as made use of HashMaps when parseing the data to not have to
    iterate the data in a double loop again for part 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,30 +1,30 @@
+use std::iter::zip;
+use std::collections::HashMap;
+
 use aoc_tools::input::input::read_lines;
 
 fn main() {
     let input = read_lines::<String>("input.txt").unwrap();
+    // Set up data structures and parse the input into them
     let mut l1: Vec::<i64> = Vec::new();
     let mut l2: Vec::<i64> = Vec::new();
+    let mut c1: HashMap<i64, i64> = HashMap::new();
+    let mut c2: HashMap<i64, i64> = HashMap::new();
     for l in input {
         let nums: Vec<i64> = l.split("   ").into_iter().map(|x| x.parse().unwrap()).collect();
+        // Add the numbers to the vectors
         l1.push(nums[0]);
         l2.push(nums[1]);
+        // Add to each number count
+        let count1 = c1.entry(nums[0]).or_default();
+        *count1 += 1;
+        let count2 = c2.entry(nums[1]).or_default();
+        *count2 += 1;
     }
+    // Order the values
     l1.sort();
     l2.sort();
-    let mut p1 = 0;
-    for i in 0..l1.len() {
-        p1 += (l1[i] - l2[i]).abs();
-    }
-    println!("part1: {}", p1);
-    let mut p2 = 0;
-    for i in 0..l1.len() {
-        let mut cntr = 0;
-        for j in 0..l2.len() {
-            if l1[i] == l2[j] {
-                cntr += 1;
-            }
-        }
-        p2 += cntr * l1[i];
-    }
-    println!("part2: {}", p2);
+    // Calculate the values
+    println!("part1: {}", zip(l1, l2).map(|(x, y)| (x-y).abs()).sum::<i64>());
+    println!("part2: {}", c1.iter().map(|(num, count)| count * num * (*c2.entry(*num).or_default())).sum::<i64>());
 }
