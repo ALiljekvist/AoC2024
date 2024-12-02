c2261137b2c9333a9c2a0a573260c8ec33d178e5
Anton Almqvist <jantonalmqvist@gmail.com>
2024-12-02 08:49:44 +0100

    Made the code in day 2 more compact
    It is basically the same solution, but done with iterators and filter
    functions instead of verbose looping and updating a mutable count.
    
    Runs a bit slower since part2 will try all possible combinations instead
    of stopping whenever we found a solution (unless the compiler is smart
    enough to do that for me).

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,17 @@
 use aoc_tools::input::input::read_lines;
 use std::iter::zip;
 
+fn is_safe(levels: &Vec<i64>) -> bool {
+    let diffs: Vec<i64> = zip(&levels[0..], &levels[1..]).map(|(x, y)| y-x).collect();
+    if diffs.iter().map(|f| f.signum()).sum::<i64>().abs() != diffs.len() as i64 {
+        return false;
+    }
+    if diffs.iter().filter(|x| x.abs() < 1 || x.abs() > 3).count() > 0 {
+        return false;
+    }
+    true
+}
+
 fn main() {
     let input = read_lines::<String>("input.txt").unwrap();
     let nums: Vec<Vec<i64>> = input.iter()
@@ -8,38 +19,13 @@ fn main() {
                                                         .map(|x| x.parse().unwrap())
                                                         .collect())
                                     .collect();
-    let mut nbr_safe = 0;
-    for levels in nums.iter() {
-        let diffs: Vec<i64> = zip(&levels[0..], &levels[1..]).map(|(x, y)| y-x).collect();
-        if diffs.iter().map(|f| f.signum()).sum::<i64>().abs() != diffs.len() as i64 {
-            continue
-        }
-        if diffs.iter().filter(|x| x.abs() < 1 || x.abs() > 3).count() > 0 {
-            continue;
-        }
-        nbr_safe += 1 ;
-    }
+    let nbr_safe = nums.iter().filter(|levels| is_safe(levels)).count();
     println!("part1: {}", nbr_safe);
-    let mut nbr_safe2 = 0;
-    for levels in nums {
-        let mut is_safe = false;
-        for i in 0..levels.len() {
-            let new_levels: Vec<i64> = levels.iter().enumerate()
-                .filter(|(i2, _)| *i2 != i)
-                .map(|(_, x)| *x).collect();
-            let diffs: Vec<i64> = zip(&new_levels[0..], &new_levels[1..]).map(|(x, y)| y-x).collect();
-            if diffs.iter().map(|f| f.signum()).sum::<i64>().abs() != diffs.len() as i64 {
-                continue
-            }
-            if diffs.iter().filter(|x| x.abs() < 1 || x.abs() > 3).count() > 0 {
-                continue;
-            }
-            is_safe = true;
-            break
-        }
-        if is_safe {
-            nbr_safe2 += 1;
-        }
-    }
+    let nbr_safe2 = nums.iter().filter(|levels|
+        (0..levels.len()).filter(|i| {
+            let sub_levels: Vec<i64> = levels.iter().enumerate().filter(|(i2, _)| i2 != i).map(|x| *x.1).collect();
+            is_safe(&sub_levels)
+            }).count() > 0
+    ).count();
     println!("part2: {}", nbr_safe2);
 }
