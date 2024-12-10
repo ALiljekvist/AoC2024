c70f99b946b8268a4df42eb73ed5291e1fd46225
Anton Almqvist <jantonalmqvist@gmail.com>
2024-12-10 22:38:04 +0100

    Optimized day9 to reduce memory usage
    Made the solution way more complex, but it should reduce the memory
    usage quite a lot. The execution time ran up from around 40ms to
    around 70ms. Still runs quite fast.
    
    One could probably solve this by calculating the checksum without
    updating the memory representation, but rather by iterating the indices
    from both ways in a smart way. That should to make it much, much faster.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,80 +1,101 @@
 use std::{fs::read_to_string, iter::zip};
 
-fn make_compact(memory: &mut Vec<i64>) {
-    let (mut i, mut j) = (0, memory.len()-1);
-    while i < j {
-        if memory[i] != -1 {
-            i += 1;
-            continue;
-        }
-        if memory[j] == -1 {
-            j -= 1;
-            continue;
-        }
-        (memory[i], memory[j]) = (memory[j], memory[i])
-    }
-}
-
-fn make_compact_blockwise(
-    memory: &mut Vec<i64>,
-    block: &(i64, usize, usize),
-    slots: &mut Vec<(usize, usize)>) {
-    // Look through all slots and try to fill 
-    for slot in slots.iter_mut() {
-        if block.1 < slot.0 {
-            return
-        }
-        if block.2 <= slot.1 {
-            // Swap memory
-            for i in 0..block.2 {
-                memory[slot.0+i] = block.0;
-                memory[block.1+i] = -1;
+fn make_compact(blocks: &Vec<(i64, usize, usize)>) -> Vec<(i64, usize, usize)> {
+    let mut new_blocks: Vec<(i64, usize, usize)> = vec![blocks[0].clone()];
+    let mut front = 1;
+    let mut back = blocks.len() - 1;
+    let mut block = blocks[back].clone();
+    while front < back {
+        let mut slot = get_slot(front, blocks);
+        while slot.1 > 0 {
+            if block.2 <= slot.1 {
+                // entire block fits, add all to the new memory layout
+                new_blocks.push((block.0, slot.0, block.2));
+                // update slot values and get next block
+                slot.0 += block.2;
+                slot.1 -= block.2;
+                back -= 1;
+                block = blocks[back].clone();
+                continue;
             }
-            // Update the refilled slot
-            slot.0 += block.2;
-            slot.1 -= block.2;
-            return
+            // fit what you can into the slot and update the remaining block before 
+            new_blocks.push((block.0, slot.0, slot.1));
+            block.2 -= slot.1;
+            slot.0 += slot.1;
+            slot.1 = 0;
         }
+        new_blocks.push(blocks[front]);
+        front += 1;
     }
+    if block.2 > 0 && block.1 == blocks[front].1 {
+        let last_block = new_blocks[new_blocks.len()-1];
+        new_blocks.push((block.0, last_block.1 + last_block.2, block.2))
+    }
+    new_blocks
 }
 
-fn check_sum(memory: &Vec<i64>) -> i64 {
-    return memory.iter()
-        .enumerate()
-        .filter(|x| *x.1 >= 0)
-        .map(|x| (x.0 as i64) * x.1)
-        .sum::<i64>()
+fn get_slot(ind: usize, blocks: &Vec<(i64, usize, usize)>) -> (usize, usize) {
+    let slot_start = blocks[ind-1].1 + blocks[ind-1].2;
+    return (slot_start, blocks[ind].1 - slot_start);
 }
 
-fn main() {
-    let input = read_to_string("input.txt").unwrap();
-    let nums: Vec<usize> = input.trim()
-        .split("")
-        .filter(|x| !x.is_empty())
-        .map(|x| x.parse().unwrap())
-        .collect();
-    let mut memory = vec![-1; nums.iter().sum()];
-    let mut blocks: Vec<(i64, usize, usize)> = Vec::new();
-    let mut mem_i = 0;
-    for (i, num) in nums.iter().enumerate() {
-        if i % 2 == 0 {
-            for j in mem_i..mem_i+num {
-                memory[j] = (i as i64)/2;
-            }
-            blocks.push(((i as i64)/2, mem_i, *num))
-        }
-        mem_i += num;
-    }
-    let mut p1_memory = memory.clone();
-    make_compact(&mut p1_memory);
-    println!("part1: {}", check_sum(&p1_memory));
-    // Create a vector of all the slots for faster iteration
+fn make_compact_blockwise(blocks: &mut Vec<(i64, usize, usize)>) {
     let mut slots: Vec<(usize, usize)> = zip(&blocks[0..], &blocks[1..]).into_iter()
         .map(|(b1, b2)| (b1.1+b1.2, b2.1-b1.1-b1.2))
         .filter(|x| x.1 != 0)
         .collect();
-    for block in blocks.iter().rev() {
-        make_compact_blockwise(&mut memory, block, &mut slots);
+    let mut i = blocks.len()-1;
+    while i > 0 {
+        let mut moved = false;
+        for slot in slots.iter_mut() {
+            if blocks[i].1 < slot.0 {
+                break
+            }
+            if blocks[i].2 <= slot.1 {
+                // take the block
+                let mut block = blocks.remove(i);
+                block.1 = slot.0;
+                // Update the refilled slot
+                slot.0 += block.2;
+                slot.1 -= block.2;
+                let mut new_ind = 0;
+                while new_ind < blocks.len() && blocks[new_ind].1 < block.1 {
+                    new_ind += 1
+                }
+                blocks.insert(new_ind, block);
+                moved = true;
+                break
+            }
+        }
+        if !moved {
+            i -= 1;
+        }
     }
-    println!("part2: {}", check_sum(&memory));
+}
+
+fn check_sum(blocks: &Vec<(i64, usize, usize)>) -> i64 {
+    return blocks.iter()
+        .map(|x| (x.1..x.1+x.2).into_iter().map(|d| d as i64).sum::<i64>() * x.0)
+        .sum::<i64>()
+}
+
+fn main() {
+    let nums: Vec<i64> = read_to_string("input.txt").unwrap()
+        .trim()
+        .split("")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let mut blocks: Vec<(i64, usize, usize)> = Vec::new();
+    let mut mem_i = 0;
+    for (i, num) in nums.iter().enumerate() {
+        if i % 2 == 0 {
+            blocks.push(((i as i64)/2, mem_i, *num as usize))
+        }
+        mem_i += *num as usize;
+    }
+    let p1_blocks = make_compact(&blocks);
+    println!("part1: {}", check_sum(&p1_blocks));
+    make_compact_blockwise(&mut blocks);
+    println!("part2: {}", check_sum(&blocks));
 }
