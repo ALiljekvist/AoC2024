6beb0d892c9d8c0359cab485f490f7119cbcaffe
Anton Almqvist <jantonalmqvist@gmail.com>
2024-12-22 17:15:21 +0100

    Finished solution for day 22
    Spotted my basic error off not appending the starting value to the list
    before starting to evolve the number. Easy fix once noticed.

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -20,10 +20,12 @@ fn evolve(secret: i64) -> i64 {
 fn main() {
     let buyers = read_lines::<i64>("input.txt").unwrap();
     let mut sequences: Vec<Vec<i64>> = vec![Vec::new(); buyers.len()];
+    // Part 1
     let p1 = buyers.iter()
         .enumerate()
         .map(|(monkey, starting_secret)| {
             let mut secret = *starting_secret;
+            sequences[monkey].push(secret%10);
             for _ in 0..2000 {
                 secret = evolve(secret);
                 sequences[monkey].push(secret%10);
@@ -31,24 +33,21 @@ fn main() {
             secret
         }).sum::<i64>();
     println!("part1: {}", p1);
-    let diffs: Vec<Vec<i64>> = sequences.iter()
-        .map(|seq| zip(&seq[0..], &seq[1..]).map(|(a,b)| b-a).collect())
-        .collect();
-    let mut possible: HashMap<&[i64], i64> = HashMap::new();
-    for (i, diff) in diffs.iter().enumerate() {
-        let mut visited: HashSet<&[i64]> = HashSet::new();
-        for (j, w) in diff.windows(4).enumerate() {
-            if j >= sequences[i].len()-4 {
-                break
-            }
-            if visited.contains(w) {
+
+    // Part 2
+    let mut patterns: HashMap<(i64, i64, i64, i64), i64> = HashMap::new();
+    for seq in sequences {
+        let mut visited: HashSet<(i64, i64, i64, i64)> = HashSet::new();
+        for w in seq.windows(5) {
+            let key = (w[1]-w[0], w[2]-w[1], w[3]-w[2], w[4]-w[3]);
+            if visited.contains(&key) {
                 continue;
             }
-            let sold = possible.entry(w).or_default();
-            *sold += sequences[i][j+4];
-            visited.insert(w);
+            let sold = patterns.entry(key).or_default();
+            visited.insert(key);
+            *sold += w[4];
         }
     }
-    let p2 = possible.values().max().unwrap();
+    let p2 = patterns.values().max().unwrap();
     println!("part2: {}", p2);
 }
