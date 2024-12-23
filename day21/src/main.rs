4ec5860be8708653dc49894674b07a395a59ca0f
Anton Almqvist <jantonalmqvist@gmail.com>
2024-12-23 21:29:47 +0100

    Full solution for day 21
    Thought I'd get away with always taking the same path between two
    buttons independent of chain level. Had to switch it out for a DFS
    looking through all the valid combinations of way to get between two
    buttons for each layer (with cache for going between the same
    combination of buttons for each layer)

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet}, iter::zip, vec};
+use std::{collections::{HashMap, HashSet}, iter::zip};
 
 use aoc_tools::input::input::read_lines;
 
@@ -35,106 +35,73 @@ fn create_input_pad_layout() -> HashMap<char, (i32, i32)> {
         ])
 }
 
-fn min_steps(a: char, b: char, layout: &HashMap<char, (i32, i32)>, positions: &HashSet<(i32, i32)>) -> String {
-    let p0 = layout.get(&a).unwrap();
-    let p1 = layout.get(&b).unwrap();
-    let dr = p1.0-p0.0;
-    let dc = p1.1-p0.1;
-    let mut new_steps: Vec<char> = Vec::new();
+fn get_possible_steps(a: (i32, i32), b: (i32, i32), valid: &HashSet<(i32, i32)>) -> Vec<String> {
+    let mut perms: Vec<String> = Vec::new();
+    let mut start: Vec<char> = Vec::from(['A']); 
+    add_remaining(&mut perms, &mut start, a, b, valid);
+    perms
+}
+
+fn add_remaining(perms: &mut Vec<String>, curr: &mut Vec<char>, a: (i32, i32), b: (i32, i32), valid: &HashSet<(i32, i32)>) {
+    if !valid.contains(&a) {
+        return
+    }
+    if a == b {
+        curr.push('A');
+        perms.push(curr.iter().collect());
+        curr.pop();
+        return
+    }
+    let dr = b.0-a.0;
+    if dr > 0 {
+        curr.push('v');
+        add_remaining(perms, curr, (a.0+1, a.1), b, valid);
+        curr.pop();
+    } else if dr < 0 {
+        curr.push('^');
+        add_remaining(perms, curr, (a.0-1, a.1), b, valid);
+        curr.pop();
+    }
+    let dc = b.1-a.1;
     if dc > 0 {
-        if positions.contains(&(p1.0, p0.1)) {
-            new_steps.append(&mut get_up_down(dr));
-            new_steps.append(&mut get_right_left(dc));
-        } else {
-            new_steps.append(&mut get_up_down(dr));
-            new_steps.append(&mut get_right_left(dc));
-        }
-    } else {
-        if !positions.contains(&(p0.0, p1.1)) {
-            new_steps.append(&mut get_up_down(dr));
-            new_steps.append(&mut get_right_left(dc));
-        } else {
-            new_steps.append(&mut get_right_left(dc));
-            new_steps.append(&mut get_up_down(dr));
-        }
+        curr.push('>');
+        add_remaining(perms, curr, (a.0, a.1+1), b, valid);
+        curr.pop();
+    } else if dc < 0 {
+        curr.push('<');
+        add_remaining(perms, curr, (a.0, a.1-1), b, valid);
+        curr.pop();
     }
-    new_steps.push('A');
-    new_steps.iter().collect()
-}
-
-fn get_up_down(dr: i32) -> Vec<char> {
-    let mut up_down = Vec::new();
-    for _ in 0..dr.abs() {
-        if dr < 0 {
-            up_down.push('^');
-        } else {
-            up_down.push('v');
-        }
-    }
-    up_down
-}
-
-fn get_right_left(dc: i32) -> Vec<char> {
-    let mut right_left = Vec::new();
-    for _ in 0..dc.abs() {
-        if dc < 0 {
-            right_left.push('<');
-        } else {
-            right_left.push('>');
-        }
-    }
-    right_left
-}
-
-fn code_to_inputs<'a>(code: &'a str, layout: &HashMap<char, (i32, i32)>) -> String {
-    let positions: HashSet<(i32, i32)> = layout.values().map(|v| *v).collect();
-    let mut path: Vec<(char, char)> = zip(code[0..].chars(), code[1..].chars()).collect();
-    // Have to insert first step as well
-    path.insert(0, ('A', code.chars().nth(0).unwrap()));
-    let mut key_presses: Vec<char> = Vec::new();
-    for step in &path {
-        let elevated_steps = min_steps(step.0, step.1, layout, &positions);
-        key_presses.append(&mut elevated_steps.chars().collect());
-    }
-    key_presses.into_iter().collect()
 }
 
 fn dfs_min_steps(
     instructions: String,
-    map: &HashMap<(char, char),String>,
-    cache: &mut HashMap<(usize, char, char),i64>,
-    level: usize,
-    max_level: usize) -> i64 {
-    if level == max_level {
-        return instructions.len() as i64
+    layout: &HashMap<char, (i32, i32)>,
+    valid: &HashSet<(i32, i32)>,
+    cache: &mut HashMap<(usize, char, char), i64>,
+    level: usize) -> i64 {
+    if level == 0 {
+        // Reached the end, return the presses (-1 due to sharing connecting chars)
+        return instructions.len() as i64 - 1
     }
-    let mut full_input = instructions.clone();
-    full_input.insert_str(0, "A");
     let mut min_steps = 0;
-    for (a, b) in zip(full_input[0..].chars(), full_input[1..].chars()) {
+    for (a, b) in zip(instructions[0..].chars(), instructions[1..].chars()) {
         if let Some(min_sub) = cache.get(&(level, a, b)) {
             min_steps += min_sub;
             continue;
         }
-        let steps = map.get(&(a,b)).unwrap().clone();
-        let min_sub = dfs_min_steps(steps, map, cache, level+1, max_level);
+        let pos_a = layout.get(&a).unwrap();
+        let pos_b = layout.get(&b).unwrap();
+        let min_sub = get_possible_steps(*pos_a, *pos_b, valid)
+                        .iter()
+                        .map(|p| dfs_min_steps(p.clone(), &layout, valid, cache, level-1))
+                        .min()
+                        .unwrap();
         cache.insert((level, a, b), min_sub);
         min_steps += min_sub;
     }
     min_steps
 }
-
-// fn find_least_button_pushes(
-//     code: &str,
-//     keypad: &HashMap<char, (i32, i32)>,
-//     input_pad: &HashMap<char, (i32, i32)>,
-//     num_input_pads: usize) -> i64 {
-//     let mut instructions = code_to_inputs(&code, &keypad);
-//     for _i in 0..num_input_pads {
-//         instructions = code_to_inputs(&instructions, input_pad);
-//     }
-//     instructions.len() as i64
-// }
 
 fn code_to_val(code: &str) -> i64 {
     code.chars().filter(|c| c.is_digit(10)).collect::<String>().parse().unwrap()
@@ -143,32 +110,32 @@ fn code_to_val(code: &str) -> i64 {
 fn main() {
     let codes = read_lines::<String>("input.txt").unwrap();
     let keypad_layout = create_keypad_layout();
+    let keypad_positions: HashSet<(i32, i32)> = keypad_layout.values().map(|v| *v).collect();
     let input_layout = create_input_pad_layout();
-    let input_positions = input_layout.values().map(|v| *v).collect();
-    let input_set = vec!['A', '^', '>', 'v', '<'];
-    let mut input_pair_mapping: HashMap<(char, char), String> = HashMap::new();
-    for a in input_set.iter() {
-        for b in input_set.iter() {
-            input_pair_mapping.insert((*a, *b),min_steps(*a, *b, &input_layout, &input_positions));
-        }
-    }
-    let mut cache: HashMap<(usize, char, char), i64> = HashMap::new();
-    let mut p1 = 0;
-    for code in &codes {
-        let b = code_to_inputs(&code, &keypad_layout);
-        let m_steps = dfs_min_steps(b, &input_pair_mapping, &mut cache, 0, 2);
-        p1 += m_steps * code_to_val(&code)
-    }
-    println!("part1: {}", p1);
+    let input_positions: HashSet<(i32, i32)> = input_layout.values().map(|v| *v).collect();
 
-    
-    let mut p2_cache: HashMap<(usize, char, char), i64> = HashMap::new();
-    let mut p2 = 0;
-    for code in &codes {
-        let b = code_to_inputs(&code, &keypad_layout);
-        let m_steps = dfs_min_steps(b, &input_pair_mapping, &mut p2_cache, 0, 25);
-        p2 += m_steps * code_to_val(&code)
-    }
-
-    println!("part2: {}", p2);
+    let all: Vec<i64> = vec![2,25].iter()
+        .map(|level| {
+        let mut cache: HashMap<(usize, char, char), i64> = HashMap::new();
+        codes.iter()
+            .map(|c| {
+                let mut tot = 0;
+                let mut code: Vec<char> = c.chars().collect();
+                code.insert(0, 'A');
+                for (a,b) in zip(&code[0..], &code[1..]) {
+                    let pos_a = keypad_layout.get(a).unwrap();
+                    let pos_b = keypad_layout.get(b).unwrap();
+                    tot += get_possible_steps(*pos_a, *pos_b, &keypad_positions)
+                        .iter()
+                        .map(|p| {
+                            dfs_min_steps(p.clone(), &input_layout, &input_positions, &mut cache, *level)
+                        })
+                        .min()
+                        .unwrap();
+                }
+                tot * code_to_val(c)
+            }).sum::<i64>()
+        }).collect();
+    println!("part1: {}", all[0]);
+    println!("part2: {}", all[1]);
 }
