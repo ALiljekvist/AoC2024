b9ee1b8399ce56c45f3d6ae11450315da2fb7149
Anton Almqvist <jantonalmqvist@gmail.com>
2024-12-25 12:06:38 +0100

    Semi-manual solution for part 2 day 24
    Figured out it was a series of logic gate adders, and decided to use the
    mappings to print out the adders into parts to see where we saw
    anomalities. Then I searched through the mappings manually to see which
    gates to switch.

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::read_to_string, time::Instant};
+use std::{collections::HashMap, fs::read_to_string};
 
 fn parse_input(input: String) -> (HashMap<String, u8>, Vec<Vec<String>>) {
     let parts: Vec<String> = input.split("\n\n")
@@ -27,10 +27,6 @@ fn get_binary_result(gates: &HashMap<String, u8>, b: char) -> u64 {
         .sum::<u64>()
 }
 
-fn from_binary(a: Vec<u8>) -> u64 {
-    return a.iter().enumerate().map(|(i, v)| *v as u64 * 2u64.pow(i as u32)).sum::<u64>();
-}
-
 fn to_binary(a: u64) -> Vec<u8> {
     let mut bins = Vec::new();
     let mut i = 0;
@@ -48,18 +44,29 @@ fn get_or_swap(a: String, swaps: &HashMap<String, String>) -> String {
     }
 }
 
+fn get_rev_map(ops: &Vec<Vec<String>>, swaps: &HashMap<String, String>) -> HashMap<String, (String, String, String)> {
+    ops.iter()
+        .map(|ps| {
+            (get_or_swap(ps[4].clone(), swaps), (ps[1].clone(), ps[0].clone(), ps[2].clone()))
+        })
+        .collect()
+}
+
+fn from_binary(a: Vec<u8>) -> u64 {
+    return a.iter().enumerate().map(|(i, v)| *v as u64 * 2u64.pow(i as u32)).sum::<u64>();
+}
+
 fn calc_gate(
     gate: &String,
     rev_map: &HashMap<String, (String, String, String)>,
     gates: &HashMap<String, u8>,
-    swaps: &HashMap<String, String>,
 ) -> u8 {
     if let Some(val) = gates.get(gate) {
         return *val
     }
     let (op, dep1, dep2) = rev_map.get(gate).unwrap();
-    let a = calc_gate(&get_or_swap(dep1.clone(), swaps), rev_map, gates, swaps);
-    let b = calc_gate(&get_or_swap(dep2.clone(), swaps), rev_map, gates, swaps);
+    let a = calc_gate(dep1, rev_map, gates);
+    let b = calc_gate(dep2, rev_map, gates);
     match op.as_str() {
         "AND" => {
             return if a + b == 2 {1} else {0};
@@ -74,32 +81,73 @@ fn calc_gate(
     };
 }
 
+// fn map_adder(gate: String, rev_map: &HashMap<String, (String, String, String)>, level: usize) -> String {
+//     if level == 0 {
+//         return gate
+//     }
+//     match gate.chars().nth(0).unwrap() {
+//         'x' => {return gate}
+//         'y' => {return gate}
+//         _ => {}
+//     }
+//     let (op, dep1, dep2) = rev_map.get(&gate).unwrap();
+//     let mut parts = vec![map_adder(dep1.clone(), rev_map, level-1), map_adder(dep2.clone(), rev_map, level-1)];
+//     parts.sort();
+//     format!("({} {} {})", parts[0], op, parts[1])
+// }
+
 fn main() {
     let input = read_to_string("input.txt").unwrap();
     let (gates, ops) = parse_input(input);
     // Part 1
-    let mut all_gates: Vec<String> = Vec::new();
-    let rev_map: HashMap<String, (String, String, String)> = ops.iter()
-        .map(|ps| {
-            all_gates.push(ps[4].clone());
-            (ps[4].clone(), (ps[1].clone(), ps[0].clone(), ps[2].clone()))
-        })
-        .collect();
+    let all_gates: Vec<String> = ops.iter().map(|op| op[4].clone()).collect();
+    // let rev_map: HashMap<String, (String, String, String)> = ops.iter()
+    //     .map(|ps| {
+    //         all_gates.push(ps[4].clone());
+    //         (ps[4].clone(), (ps[1].clone(), ps[0].clone(), ps[2].clone()))
+    //     })
+    //     .collect();
+    let rev_map = get_rev_map(&ops, &HashMap::new());
     let mut z_gates: Vec<String> = all_gates.iter()
         .filter_map(|s| if s.chars().nth(0).unwrap() == 'z' {Some(s.clone())} else {None})
         .collect();
     z_gates.sort();
-    let now = Instant::now();
-    let finish: Vec<u8> = z_gates.iter().map(|gate| calc_gate(gate, &rev_map, &gates, &HashMap::new())).collect();
-    println!("part1: {} ({:?})", from_binary(finish), now.elapsed());
+    let finish: Vec<u8> = z_gates.iter().map(|gate| calc_gate(gate, &rev_map, &gates)).collect();
+    println!("part1: {}", from_binary(finish));
 
-    // Part 2
-    let wanted = get_binary_result(&gates, 'x') + get_binary_result(&gates, 'y');
-    let mut wanted_bin = to_binary(wanted);
-    while wanted_bin.len() < z_gates.len() {
-        wanted_bin.push(0);
-    }
+    // Part 2:
+    let wanted_bin = to_binary(get_binary_result(&gates, 'x') + get_binary_result(&gates, 'y'));
     // Realize that the input of earlier z-gates affect the later z-gates, which means
-    // they should follow a pattern. Probably have to swap all z-gates that is not
-    // calculated with an XOR.
+    // they should follow a pattern. Seems to be logic adders with a carry-over from
+    // previous gates.
+    let mut swaps: HashMap<String, String> = HashMap::new();
+    for (p1, p2) in vec![("z05", "tst"), ("z11", "sps"), ("z23","frt"), ("cgh", "pmd")] {
+        swaps.insert(p1.to_string(), p2.to_string());
+        swaps.insert(p2.to_string(), p1.to_string());
+    }
+    //  Create reverse map and check that the swaps made the logic work
+    let new_rev_map = get_rev_map(&ops, &swaps);
+    let p2_finish: Vec<u8> = z_gates.iter().map(|gate| calc_gate(gate, &new_rev_map, &gates)).collect();
+    if (0..p2_finish.len()).map(|i| if p2_finish[i] != wanted_bin[i] {1} else {0}).sum::<i32>() > 0 {
+        println!("Not correct");
+    }
+
+    // Manually find pairs by printing the patterns and see where it doesn't match.
+    // Try to build the sum part of the adder for each z-gate, and print the parts.
+
+    // Uncomment to print what was used to manually search through the input.
+    // for gate in z_gates.iter() {
+    //     println!("{}: {}", gate, map_adder(gate.clone(), &new_rev_map, 2))
+    // }
+    // // Also check the dep-chain for all carry-overs (Should all have OR-operations)
+    // for (co, (op, _, _)) in new_rev_map.iter() {
+    //     if op != "OR" {
+    //         continue;
+    //     }
+    //     println!("{}: {}", co, map_adder(co.clone(), &new_rev_map, 2));
+    // }
+
+    let mut sorted_swaps: Vec<String> = swaps.keys().map(|s| s.clone()).collect();
+    sorted_swaps.sort();
+    println!("part2: {}", sorted_swaps.join(","));
 }
